3ea800516d34f3e0422d50fbba444966d99baf5c
Motahare <motsai@alu.edu.gva.es>
2023-05-30 21:35:00 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <Title :title="homeTitle"/>
+  </div>
+</template>
+
+<script setup>
+import Title from '../components/Title.vue'
+const homeTitle = "Hello from home view"
+</script>
